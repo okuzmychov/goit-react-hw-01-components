585ad256e3d03860531295207938d3c273063a55
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-08-18 18:14:11 +0300

    friendlist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from './Profile';
+import userProfile from '../user.json';
+import { Statistics } from './Statistics';
+import data from '../data.json';
+import { FriendList } from './FriendList';
+import friends from '../friends.json';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <Profile user={userProfile} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
---
+++ src/components/FriendList.js
@@ -0,0 +1,13 @@
+export const FriendList = () {
+    return (
+    <div>
+            <ul class="friend-list">
+                <li class="item">
+        <span class="status"></span>
+        <img class="avatar" src="" alt="User avatar" width="48" />
+        <p class="name"></p>
+        </li>
+        </ul>
+        </div>;
+    );
+};
