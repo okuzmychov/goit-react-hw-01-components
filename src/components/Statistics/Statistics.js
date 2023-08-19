14f6542c20a8a9be2f0ba5b33924f278944f5730
okuzmychov <o.kuzmichev@gmail.com>
2023-08-19 21:46:52 +0300

    statistic

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,13 +2,16 @@ import { StatCard } from './Statcard'
 
 export const Statistics = ({ stats }) => {
   return (
+    <div>
+    <h3>Upload stats</h3>
       <ul className="stat-list">
         {stats.map(stat => (
         <li key={stat.id}>
           <StatCard quiz={stat} />
         </li>
       ))}
-    </ul>
+      </ul>
+      </div>
   );
 };
 
