25581bd90c7bcb1a0269ce6d53abac0615dbabe8
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-08-22 11:42:34 +0300

    transactions

--- src/components/TransactionHistory/TransactionHistory.styled.js
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -1,10 +1,11 @@
 import { styled } from 'styled-components';
 
 export const Table = styled.table`
-  background-color: ${p => p.theme.colors.grey};
+  background-color: ${p => p.theme.colors.white};
   border-radius: 4px;
   overflow: hidden;
   width: 50%;
+  border: 1px solid ${props => props.theme.colors.grey};
 `;
 
 export const List = styled.th`
@@ -14,8 +15,16 @@ export const List = styled.th`
   text-align: left;
   background-color: ${p => p.theme.colors.verbigris};
   color: ${p => p.theme.colors.white};
+  :hover {
+    background-color: #f5f5f5;
+  }
 `;
 
 export const ListEl = styled.td`
   padding: 12px;
+  border: 1px solid ${props => props.theme.colors.grey};
+
+  :nth-child(even) {
+    background-color: ${p => p.theme.colors.white};
+  }
 `;
